f3673588b25fcc055c5e9396141a44e3118da755
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-01 20:42:09 +0000

    Refactor day one

--- lib/one.ex
+++ lib/one.ex
@@ -1,51 +1,32 @@
 defmodule One do
 
-  def process_coordinates([[rotation | steps]]) when rotation === 82 do
-    {List.to_integer(steps), 0}
-  end
+  @left 76
+  @right 82
 
-  def process_coordinates([[rotation | steps]]) when rotation === 76 do
-    {-List.to_integer(steps), 0}
-  end
-
-  def process_coordinates([[rotation | steps] | future_instructions]) when rotation === 82 do
+  def process_coordinates([[rotation | steps] | future_instructions]) when rotation === @right do
     process_coordinates(future_instructions, {List.to_integer(steps), 0}, :east) 
   end
 
-  def process_coordinates([[rotation | steps] | future_instructions]) when rotation === 76 do
+  def process_coordinates([[rotation | steps] | future_instructions]) when rotation === @left do
     process_coordinates(future_instructions, {-List.to_integer(steps), 0}, :west) 
   end
 
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :east) when rotation === 76 do
-    process_coordinates(future_instructions, {current_x, current_y + List.to_integer(steps)}, :north)
+  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, direction) when rotation === @left do
+    case direction do 
+      :east -> process_coordinates(future_instructions, {current_x, current_y + List.to_integer(steps)}, :north)
+      :north -> process_coordinates(future_instructions, {current_x - List.to_integer(steps), current_y}, :west)
+      :west -> process_coordinates(future_instructions, {current_x, current_y - List.to_integer(steps)}, :south)
+      :south -> process_coordinates(future_instructions, {current_x + List.to_integer(steps), current_y}, :east)
+    end
   end
 
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :north) when rotation === 76 do
-    process_coordinates(future_instructions, {current_x - List.to_integer(steps), current_y}, :west)
-  end
-
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :west) when rotation === 76 do
-    process_coordinates(future_instructions, {current_x, current_y - List.to_integer(steps)}, :south)
-  end
-
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :south) when rotation === 76 do
-    process_coordinates(future_instructions, {current_x + List.to_integer(steps), current_y}, :east)
-  end
-
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :east) when rotation === 82 do
-    process_coordinates(future_instructions, {current_x, current_y - List.to_integer(steps)}, :south)
-  end
-
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :north) when rotation === 82 do
-    process_coordinates(future_instructions, {current_x + List.to_integer(steps), current_y}, :east)
-  end
-
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :west) when rotation === 82 do
-    process_coordinates(future_instructions, {current_x, current_y + List.to_integer(steps)}, :north)
-  end
-
-  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, :south) when rotation === 82 do
-    process_coordinates(future_instructions, {current_x - List.to_integer(steps), current_y}, :west)
+  def process_coordinates([[rotation | steps] | future_instructions] , {current_x, current_y}, direction) when rotation === @right do
+    case  direction do
+      :east -> process_coordinates(future_instructions, {current_x, current_y - List.to_integer(steps)}, :south)
+      :north -> process_coordinates(future_instructions, {current_x + List.to_integer(steps), current_y}, :east)
+      :west -> process_coordinates(future_instructions, {current_x, current_y + List.to_integer(steps)}, :north)
+      :south -> process_coordinates(future_instructions, {current_x - List.to_integer(steps), current_y}, :west)
+    end
   end
 
   def process_coordinates([], coordinates, _direction) do
